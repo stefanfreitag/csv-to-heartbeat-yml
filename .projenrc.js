05c7fcf7d0ad96a57a5330041bc9e379b90a7be4
Stefan Freitag <stefan.freitag@udo.edu>
2021-04-25 22:18:42 +0200

    fix: Added ruby bundles for now

--- .projenrc.js
+++ .projenrc.js
@@ -1,24 +1,21 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, ProjectType } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.100.0',
-  name: 'cdk',
-  cdkDependencies: [
-    '@aws-cdk/aws-apigateway',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-logs',
-  ],
-  authorEmail: 'stefan@stefreitag.de',
-  authorName: 'Stefan Freitag',
-  license: 'Apache-2.0',
-  description: 'Converts CSV data to heartbeat yml files.',
-  defaultReleaseBranch: 'master',
-  workflowBootstrapSteps: [
-    'yarn install--frozen - lockfile && yarn projen',
-    'chmod o + x. / installLayerDependencies.sh && sh. / installLayerDependencies.sh',
-  ],
+    cdkVersion: '1.100.0',
+    name: 'cdk',
+    cdkDependencies: [
+        '@aws-cdk/aws-apigateway',
+        '@aws-cdk/aws-lambda',
+        '@aws-cdk/aws-logs',
+    ],
+    authorEmail: 'stefan@stefreitag.de',
+    authorName: 'Stefan Freitag',
+    license: 'Apache-2.0',
+    description: 'Converts CSV data to heartbeat yml files.',
+    defaultReleaseBranch: 'master',
+    projectType: ProjectType.APP,
 });
 
-project.npmignore.exclude('data.csv', 'assets/vendor/*');
-project.gitignore.exclude('data.csv', 'assets/vendor/*');
+//project.npmignore.exclude('data.csv', 'assets/vendor/*');
+//project.gitignore.exclude('data.csv', 'assets/vendor/*')
 project.synth();
