7f16a1f563f121d0ea64412630715c0ada0ef49e
Stefan Freitag <stefan.freitag@udo.edu>
2021-04-25 21:57:55 +0200

    fix: Added setup lambda dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -13,7 +13,13 @@ const project = new AwsCdkTypeScriptApp({
   license: 'Apache-2.0',
   description: 'Converts CSV data to heartbeat yml files.',
   defaultReleaseBranch: 'master',
+  scripts: {
+    'install-layer-dependencies': 'chmod o+x ./installLayerDependencies.sh;sh ./installLayerDependencies.sh',
+  },
 });
+
+const npmScripts = project.tryFindObjectFile('package.json');
+npmScripts.addOverride('scripts.build', 'npx projen install-layer-dependencies; npx projen build');
 
 project.npmignore.exclude('data.csv', 'assets/vendor/*');
 project.gitignore.exclude('data.csv', 'assets/vendor/*');
