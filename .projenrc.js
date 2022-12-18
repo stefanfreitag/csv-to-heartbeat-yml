2dc72288d7a7e625773cfe7d2fd2dc0615f4074d
Stefan Freitag <stefan.freitag@rwe.com>
2022-12-18 10:31:31 +0100

    chore: changed to monthly update schedule

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,25 @@
-const { awscdk } = require('projen');
-const { Stability } = require('projen/lib/cdk');
+const { awscdk } = require("projen");
+const { Stability } = require("projen/lib/cdk");
+const { UpgradeDependenciesSchedule } = require("projen/lib/javascript");
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.55.1',
-  name: 'cdk',
-  authorEmail: 'stefan.freitag@ud.edu',
-  authorName: 'Stefan Freitag',
-  license: 'Apache-2.0',
-  description: 'Converts CSV data to heartbeat yml files.',
-  defaultReleaseBranch: 'master',
+  cdkVersion: "2.55.1",
+  name: "csv_to_heartbeat",
+  authorEmail: "stefan.freitag@udo.edu",
+  authorName: "Stefan Freitag",
+  stability: Stability.EXPERIMENTAL,
+  license: "Apache-2.0",
+  defaultReleaseBranch: "master",
+  depsUpgradeOptions: {
+    workflowOptions: {
+      schedule: UpgradeDependenciesSchedule.MONTHLY,
+    },
+  },
 });
 
-project.npmignore.exclude('data.csv', 'assets/vendor/*', '.history');
-project.gitignore.exclude('data.csv', 'assets/vendor/*', '.history');
+const common_exclude = ["data.csv", "assets/vendor/*", ".history"];
+
+project.gitignore.exclude(...common_exclude);
+project.npmignore.exclude(...common_exclude);
+
 project.synth();
