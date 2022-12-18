032b34e9512ec3a0c0d1a37997b2eda2d21110ac
Stefan Freitag <stefan.freitag@rwe.com>
2022-12-18 10:34:56 +0100

    fix: updated formatting

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,15 @@
-const { awscdk } = require("projen");
-const { Stability } = require("projen/lib/cdk");
-const { UpgradeDependenciesSchedule } = require("projen/lib/javascript");
+const { awscdk } = require('projen');
+const { Stability } = require('projen/lib/cdk');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.55.1",
-  name: "csv_to_heartbeat",
-  authorEmail: "stefan.freitag@udo.edu",
-  authorName: "Stefan Freitag",
+  cdkVersion: '2.55.1',
+  name: 'csv_to_heartbeat',
+  authorEmail: 'stefan.freitag@udo.edu',
+  authorName: 'Stefan Freitag',
   stability: Stability.EXPERIMENTAL,
-  license: "Apache-2.0",
-  defaultReleaseBranch: "master",
+  license: 'Apache-2.0',
+  defaultReleaseBranch: 'master',
   depsUpgradeOptions: {
     workflowOptions: {
       schedule: UpgradeDependenciesSchedule.MONTHLY,
@@ -17,7 +17,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   },
 });
 
-const common_exclude = ["data.csv", "assets/vendor/*", ".history"];
+const common_exclude = ['data.csv', 'assets/vendor/*', '.history'];
 
 project.gitignore.exclude(...common_exclude);
 project.npmignore.exclude(...common_exclude);
